b3373d5c7b1871f3c5b3f3a8f4a61155e8e98842
hulinbig <hulinbig>
2020-12-11 17:43:08 +0800

    更新

--- apps/user/views.py
+++ apps/user/views.py
@@ -11,3 +11,23 @@ user_bp1 = Blueprint('user', __name__, url_prefix='/user')
 def index():
     return render_template('base.html')
 
+@user_bp1.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        repassword = request.form.get('repassword')
+        phone = request.form.get('phone')
+        email = request.form.get('email')
+        if password == repassword:
+            user = User()
+            user.username = username
+            user.password = hashlib.md5(password.encode('utf-8')).hexdigest()
+            user.phone = phone
+            user.email = email
+            db.session.add(user)
+            db.session.commit()
+            return '注册成功'
+        else:
+            return '二次密码不一致'
+    return render_template('user/register.html')
